8e4a7070695642ada8746931ce6aa07522362dfd
BigSorry <lexmeulenkamp@hotmail.nl>
2019-04-01 14:29:48 +0200

    difference set

--- Main.py
+++ Main.py
@@ -17,6 +17,7 @@ friendsData = friendsData.values
 
 transactionsData = transactions.loc[:,0:2]
 transactionsData.loc[:,0] =  transactionsData.loc[:,0].apply(lambda x : int(time.mktime(time.strptime(x,"%Y-%m-%d %H:%M:%S"))))
+transactionsDataP = transactionsData
 transactionsData = transactionsData.values
 
 # graphFriends.add_edges_from(friendsData.loc[:, 1:2].values)
@@ -25,22 +26,28 @@ transactionsData = transactionsData.values
 edgesFriends = defaultdict(set)
 for [date,node1,node2] in friendsData:
     key = (min(node1,node2), max(node1,node2))
-    # if key in edgesFriends:
     edgesFriends[key].add(date)
-    # else:
-    #     edgesFriends[key] = set([date])
 
 # edgesTrans = np.array([val for val in graphTrans.edges.keys()])
 # Friends dups
 intersect = []
+difference = []
 for [date,node1,node2] in transactionsData:
     key = (min(node1,node2), max(node1,node2))
     if key in edgesFriends:
+        boolIntersect = 0
         for dateFriend in edgesFriends[key]:
             if date > dateFriend:
                 intersect.append((date, min(node1,node2), max(node1,node2)))
+                boolIntersect = 1
                 break
-print(len(intersect))
-test = np.unique(intersect, axis =0)
-print(len(test))
-print(graphFriends.edges)
+        if boolIntersect == 0:
+            difference.append((date, min(node1,node2), max(node1,node2)))
+    else:
+        difference.append((date, min(node1,node2), max(node1,node2)))
+
+intersect = np.unique(intersect, axis = 0)
+
+print("Prob is {0}".format(np.array(difference).shape[0] / transactionsData.shape[0]))
+print("Prob is {0}".format((transactionsData.shape[0] - intersect.shape[0]) / transactionsData.shape[0]))
+print('Done')
